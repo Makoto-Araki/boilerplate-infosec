c548660a689cafc41bda1ac1fe067f10fa0ade6f
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-12-12 01:05:01 +0000

    2022年12月12日 - 01

--- myApp.js
+++ myApp.js
@@ -1,38 +1,58 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
+// ヘッダー(X-Powered-By)を削除
+app.use(helmet.hidePoweredBy());
 
+// クリックジャッキングのリスク軽減
+app.use(
+  helmet.frameguard({
+    action: 'deny',
+  })
+);
 
+// クロスサイトスクリプティング(XSS)のリスク軽減
+app.use(helmet.xssFilter());
 
+// 応答MIMEタイプの推測を避ける
+app.use(helmet.noSniff())
 
+// IEが信頼できないHTMLを開くのを防ぐ
+app.use(helmet.ieNoOpen())
 
+// セキュリティポリシー(HSTS)の設定をオーバーライド
+app.use(helmet.hsts({
+  maxAge: 90*24*60*60,  // 90日
+  force: true,  // 今後90日間はHTTPS通信を強制
+}))
 
+// DNSレコードのプリフェッチを抑止
+app.use(helmet.dnsPrefetchControl())
 
+// ブラウザのキャッシュ無効 => 常にHTMLダウンロード
+app.use(helmet.noCache())
 
+// コンテンツセキュリティポリシー(CSP)を設定
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com'],
+  },
+}))
 
+// デフォルト値で一括設定
+// app.use(helmet())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 複数設定をオーバーライド
+// app.use(helmet({
+//   contentSecurityPolicy: {
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       styleSrc: ['style.com'],
+//     }
+//   },
+// }))
 
 
 
